840cb6d7665067d0c3bef43d6e399c6004c0d8bd
Abbey <abbeysantos1210@gmail.com>
2025-04-20 18:50:57 +0800

    chore: add GithubUserActivityApplication

--- src/main/java/com/example/github_user_activity/GithubUserActivityApplication.java
+++ src/main/java/com/example/github_user_activity/GithubUserActivityApplication.java
@@ -3,11 +3,31 @@ package com.example.github_user_activity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+
+
 @SpringBootApplication
-public class GithubUserActivityApplication {
+public class GithubUserActivityApplication implements CommandLineRunner{
+	private final GithubActivityService activityService;
+
+	public GithubUserActivityApplication(GithubActivityService activityService) {
+		this.activityService = activityService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubUserActivityApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) {
+		if (args.length == 0) {
+			System.out.println("Usage: github-activity <username>");
+			return;
+		}
+
+		String username = args[0];
+		activityService.fetchAndDisplayActivity(username);
+	}
+
 
 }
